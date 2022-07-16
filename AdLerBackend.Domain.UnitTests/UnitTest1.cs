a1fd9b1c9ee1356cbfb040e5ed83a945d1c45e01
philgei <philipp@geirhos.com>
2022-07-16 14:47:44 +0200

    initial Project structure

---
+++ AdLerBackend.Domain.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace AdLerBackend.Domain.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ AdLerBackend.Infrastructure.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace AdLerBackend.Infrastructure.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
