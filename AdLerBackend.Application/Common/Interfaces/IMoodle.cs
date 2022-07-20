fda67317502970f1f5627b3405910700dde441e1
philgei <philipp@geirhos.com>
2022-07-20 11:50:05 +0200

    added interface for moodle

---
+++ AdLerBackend.Application/Common/Interfaces/IMoodle.cs
@@ -0,0 +1,10 @@
+﻿namespace AdLerBackend.Application.Common.Interfaces;
+
+public interface IMoodle
+{
+    public MoodleTokenDTO GetToken(string username, string password);
+}
+
+public class MoodleTokenDTO
+{
+}
